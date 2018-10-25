391c957489f77d802d08753570bcfae0c666a8da
Alexey-t <support@uvviewsoft.com>
2018-10-25 12:01:18 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,30 @@
+import os
+from cudatext import *
+
+fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_typewriter.ini')
+
+def bool_to_str(v): return '1' if v else '0'
+def str_to_bool(s): return s=='1'
+
+class Command:
+    
+    def on(self):
+    
+        app_proc(PROC_SET_EVENTS, 'cuda_auto_center_line;on_caret;;')
+        msg_status('Auto Center Line activated')
+
+    def off(self):
+
+        app_proc(PROC_SET_EVENTS, 'cuda_auto_center_line;;;')
+        msg_status('Auto Center Line deactivated')
+
+    def on_caret(self, ed_self):
+
+        carets = ed.get_carets()
+        if len(carets)!=1:
+            return
+            
+        x, y, x1, y1 = carets[0]
+        h = ed.get_prop(PROP_VISIBLE_LINES)
+        
+        ed.set_prop(PROP_SCROLL_VERT, max(0, y-h//2) )
---
+++ readme/readme.txt
@@ -0,0 +1,11 @@
+plugin for CudaText.
+when active, puts line with caret always to the center of the editor window.
+
+plugin gives 2 commands in the menu Plugins - Auto Center Line:
+  Activate (you must call it for plugin to work)
+  Deactivate
+plugin is not activated on CudaText restart.
+plugin is passive when multi-carets are placed.
+
+author: Alexey (CudaText)
+license: MIT
