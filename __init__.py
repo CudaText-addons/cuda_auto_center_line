4a478f4a0a6aa4181293a6d7749a7f2d830227f8
Alexey <support@uvviewsoft.com>
2023-03-06 19:25:28 +0300

    patch from Markus

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 import os
 from cudatext import *
 
+from cudax_lib import get_translation
+_ = get_translation(__file__)  # I18N
+
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'plugins.ini')
 SECTION = 'auto_center_line'
 
@@ -10,42 +13,45 @@ def str_to_bool(s): return s=='1'
 class Command:
     act = False
     h = 50
-    
+
     def config(self):
 
         ini_write(fn_config, SECTION, 'h', str(self.h))
         file_open(fn_config)
+        # Jump to section
+        lines = [line.strip() for line in ed.get_text_all().splitlines()]
+        ed.set_caret(0, lines.index('['+SECTION+']'))
 
     def toggle(self):
-        
+
         self.act = not self.act
         if self.act:
             self.h = int(ini_read(fn_config, SECTION, 'h', str(self.h)))
             app_proc(PROC_SET_EVENTS, 'cuda_auto_center_line;on_caret;;')
-            msg_status('Auto Center Line activated')
+            msg_status(_('Auto Center Line activated'))
         else:
             app_proc(PROC_SET_EVENTS, 'cuda_auto_center_line;;;')
-            msg_status('Auto Center Line deactivated')
+            msg_status(_('Auto Center Line deactivated'))
 
     def on_caret(self, ed_self):
-    
+
         carets = ed.get_carets()
         if len(carets)!=1:
             return
-            
+
         x, y, x1, y1 = carets[0]
         if y1>=0:
             return
-        
+
         # handle mouse selection
         st = app_proc(PROC_GET_KEYSTATE, '')
         if 'L' in st or 'R' in st:
             return
-        
+
         h = ed.get_prop(PROP_VISIBLE_LINES)
         h_delta = h * self.h // 100
         pos = max(0, y-h_delta)
-        
+
         if ed.get_prop(PROP_WRAP)!=0:
             w = ed.get_wrapinfo()
             for n in reversed(range(len(w))):
@@ -53,5 +59,5 @@ class Command:
                 if wi['line']==y and wi['char']-1<=x:
                     pos = max(0, n-h_delta)
                     break
-        
+
         ed.set_prop(PROP_SCROLL_VERT, pos)
